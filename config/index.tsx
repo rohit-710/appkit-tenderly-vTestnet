4fa2838a1fa1a87c9dfddd111e992e3fb8f6caf4
Rohit Ramesh <rrohitramesh710@gmail.com>
2024-08-28 14:15:39 +0200

    removing unnecessary import

--- config/index.tsx
+++ config/index.tsx
@@ -1,5 +1,4 @@
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { authConnector } from "@web3modal/wagmi";
 
 import { cookieStorage, createStorage } from "wagmi";
 import { scroll, scrollSepolia } from "wagmi/chains";
@@ -10,9 +9,9 @@ export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 if (!projectId) throw new Error("Project ID is not defined");
 
 export const metadata = {
-  name: "appkit-example-app",
-  description: "AppKit Example",
-  url: "https://web3modal.com", // origin must match your domain & subdomain
+  name: "appkit-example-scroll",
+  description: "AppKit Example - Scroll",
+  url: "https://scrollapp.com", // origin must match your domain & subdomain
   icons: ["https://avatars.githubusercontent.com/u/37784886"]
 };
 
